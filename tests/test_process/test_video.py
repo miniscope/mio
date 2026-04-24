04a8aabc5bf974d076b21f8f3b9d16ada5ed75b3
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-04-23 22:37:28 -0700

    whoops removed the wrong test

--- tests/test_process/test_video.py
+++ tests/test_process/test_video.py
@@ -45,16 +45,14 @@ def test_noise_patch_processor(video_frame, tmp_path):
     assert processor.name == "denoise_example"
     assert processor.output_enable
 
-
-def test_noise_patch_processor_no_config(random_8bit_video_frame, tmp_path):
+def test_noise_patch_processor_no_methods(random_8bit_video_frame, tmp_path):
     denoise_config = DenoiseConfig.from_id("denoise_example")
     denoise_config.noise_patch.enable = True
-    denoise_config.noise_patch.gradient_config = None
-    denoise_config.noise_patch.black_area_config = None
+    denoise_config.noise_patch.method = []
 
-    # This should raise a ValueError because the necessary configs are not provided
-    with pytest.raises(ValueError):
-        NoisePatchProcessor("denoise_example", denoise_config.noise_patch, tmp_path)
+    processor = NoisePatchProcessor("denoise_example", denoise_config.noise_patch, tmp_path)
+    processed_frame = processor.process_frame(random_8bit_video_frame, 0)
+    assert processed_frame is random_8bit_video_frame
 
 
 def test_freqency_mask_processor(video_frame, tmp_path):
