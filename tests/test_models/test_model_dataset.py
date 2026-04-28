204d887cce0f8cad387f197098770ac9b9f405ea
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2026-04-27 20:45:29 -0700

    proper indexing and assignment in trim metadata test

--- tests/test_models/test_model_dataset.py
+++ tests/test_models/test_model_dataset.py
@@ -10,14 +10,13 @@ from ..conftest import DATA_DIR
 def test_recording_trim_metadata():
     vid_path = DATA_DIR / "stitch" / "video1.avi"
     metadata = pd.read_csv(vid_path.with_suffix(".csv"))
-    metadata_list = metadata.values.tolist()
-    metadata_list.append(metadata_list[-1])
-    metadata = pd.DataFrame(metadata_list, columns=metadata.columns)
-    metadata.iloc[-1]["reconstructed_frame_index"] += 1
+    metadata.loc[metadata.index[-1], "reconstructed_frame_index"] += 1
     vid = VideoProxy(vid_path)
     n_frames = vid.shape[0]
+
+    assert int(metadata["reconstructed_frame_index"].max()) == n_frames
 
     with pytest.warns(UserWarning):
         recording = Recording(name="video1", type="raw", video=vid_path, metadata=metadata)
 
-    assert recording.metadata["reconstructed_frame_index"].max() == n_frames - 1
+    assert int(recording.metadata["reconstructed_frame_index"].max()) == n_frames - 1
